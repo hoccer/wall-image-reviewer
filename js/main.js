516c4961885f6bba31bdedc2ced729a98d10dafb
Jens Eraßmy <jens.erassmy@artcom.de>
2014-07-24 15:18:55 +0200

    Use model classes from talk-webclient-js-model dependency

--- js/main.js
+++ js/main.js
@@ -4,16 +4,16 @@ var Backbone = require('backbone');
 var jQuery = require('jquery');
 var React = require('react');
 
-var CollectionUpdater = require('./collection-updater');
 var config = require('./config');
-var DownloadCollection = require('./models/download-collection');
 var imageGridView = require('./views/image-grid-view');
+
+var TalkWebClientModel = require('talk-webclient-js-model');
 
 // Help Backbone find jQuery
 Backbone.$ = jQuery;
 
 // Initialize image collection
-var images = new DownloadCollection();
+var images = new TalkWebClientModel.Model.DownloadCollection();
 images.fetch({data: {mediaType: 'image'}});
 
 // Update image collection with WebSocket updates
@@ -24,7 +24,9 @@ var updateUrl = function(backendUrl) {
   return url + '/updates';
 };
 
-var updater = new CollectionUpdater(updateUrl(config.backendUrl));
+var updater = new TalkWebClientModel.WebSocket.CollectionUpdater(
+	updateUrl(config.backendUrl)
+);
 updater.subscribe('/api/downloads', images, function(download) {
   return download.mediaType === 'image';
 });
