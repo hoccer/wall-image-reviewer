d4a38a2e6e880489b2a5808b64aa5c614ed9dcb5
Jens Eraßmy <jens.erassmy@artcom.de>
2014-07-25 11:34:39 +0200

    Adapt talk-webclient-js-model changes

--- js/main.js
+++ js/main.js
@@ -3,18 +3,17 @@
 var Backbone = require('backbone');
 var jQuery = require('jquery');
 var React = require('react');
+var WebClient = require('talk-webclient-js-model');
 
 var config = require('./config');
 var imageGridView = require('./views/image-grid-view');
-
-var TalkWebClientModel = require('talk-webclient-js-model');
 
 // Help Backbone find jQuery
 Backbone.$ = jQuery;
 
 // Initialize image collection
-var images = new TalkWebClientModel.Model.DownloadCollection({}, {
-	backendUrl: config.backendUrl
+var images = new WebClient.Model.DownloadCollection(null, {
+  backendUrl: config.backendUrl
 });
 images.fetch({data: {mediaType: 'image'}});
 
@@ -26,7 +25,7 @@ var updateUrl = function(backendUrl) {
   return url + '/updates';
 };
 
-var updater = new TalkWebClientModel.WebSocket.CollectionUpdater(
+var updater = new WebClient.WebSocket.Observer(
 	updateUrl(config.backendUrl)
 );
 updater.subscribe('/api/downloads', images, function(download) {
