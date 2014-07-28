20929c929e3ed898d75e8662da0284a39cb75f71
Guido Lorenz <guido.lorenz@artcom.de>
2014-07-28 16:21:37 +0200

    Add previous and next buttons

--- js/components/image-approval-component.js
+++ js/components/image-approval-component.js
@@ -8,17 +8,47 @@ var imageComponent = require('./image-component');
 module.exports = React.createClass({
   mixins: [BackboneReactComponent.mixin],
 
+  nextButton: function(index) {
+    if (index > 0) {
+      var previousImage = this.getCollection().at(index - 1);
+
+      return React.DOM.a({
+        className: 'btn btn-default',
+        href: '/images/' + previousImage.id
+      }, 'Next');
+    }
+
+    return null;
+  },
+
+  previousButton: function(index) {
+    if (index < this.getCollection().length - 1) {
+      var nextImage = this.getCollection().at(index + 1);
+
+      return React.DOM.a({
+        className: 'btn btn-default',
+        href: '/images/' + nextImage.id
+      }, 'Previous');
+    }
+
+    return null;
+  },
+
   render: function() {
-    var image = this.getCollection().get(this.props.imageId);
+    var collection = this.getCollection();
+    var imageId = this.props.imageId;
+
+    var image = collection.get(imageId);
+    var index = collection.indexOf(image);
 
     var content = image ?
                   imageComponent({model: image}) :
                   'No image with ID ' + this.props.imageId;
 
     return React.DOM.div({className: 'row fullscreen'}, [
-      React.DOM.div({className: 'col-md-1'}),
+      React.DOM.div({className: 'col-md-1'}, this.previousButton(index)),
       React.DOM.div({className: 'col-md-10 text-center'}, content),
-      React.DOM.div({className: 'col-md-1'})
+      React.DOM.div({className: 'col-md-1'}, this.nextButton(index))
     ]);
   }
 });
