474183d614aa77ebd0e03851ba6cde4247094969
Guido Lorenz <guido.lorenz@artcom.de>
2014-07-30 12:21:29 +0200

    Remove unnecessary forceUpdate calls

--- js/components/root-component.js
+++ js/components/root-component.js
@@ -7,14 +7,6 @@ var imageComponent = require('./image-component');
 var navigationComponent = require('./navigation-component');
 
 module.exports = React.createClass({
-  componentWillMount: function() {
-    this.props.imageCollection.on('change', this.forceUpdate, this);
-  },
-
-  componentWillUnmount: function() {
-    this.props.imageCollection.off('change', this.forceUpdate, this);
-  },
-
   render: function() {
     return React.DOM.div({className: 'fullscreen'}, [
       navigationComponent({
