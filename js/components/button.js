4c539f064e2db81db914f1c026c28f8cf789310a
Guido Lorenz <guido.lorenz@artcom.de>
2014-07-31 08:25:55 +0200

    Use getDefaultProps() in button

--- js/components/button.js
+++ js/components/button.js
@@ -3,12 +3,18 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  getDefaultProps: function() {
+    return {
+      color: 'default'
+    };
+  },
+
   render: function() {
     return React.DOM.button({
       type: 'button',
       className: [
         'btn',
-        'btn-' + (this.props.color || 'default'),
+        'btn-' + (this.props.color),
         'position-' + this.props.position
       ].join(' '),
       onClick: this.props.onClick
